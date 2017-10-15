247a7df87721f2f1ad113db3ac4e97016f7a6540
Renato Ribeiro <re95.renato@gmail.com>
2017-10-14 23:45:36 -0300

    copiando dados para novo arquivo funcionando

--- exemploWav.c
+++ exemploWav.c
@@ -1,87 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct{
-//Buffers e chunks...
-char ChunkID[4], Format[4], Subchunk1ID[4], Subchunk2ID[4];
-int ChunkSize, Subchunk1Size, SampleRate, ByteRate, Subchunk2Size;
-short AudioFormat, NumChannels, BlockAlign, BitsPerSample;
-short *Data;
-}headerWAV;
-short* createData(FILE *input, headerWAV header){
- short *Data = (short*) malloc(sizeof(short)*(header.Subchunk2Size)*(header.BitsPerSample/8));
- return Data;
+
+typedef struct tipoWav{
+	char ChunkID[4];
+	int ChunkSize;
+	char Format[4];
+	char Subchunk1ID[4];
+	int Subchunk1Size;
+	short AudioFormat;
+	short NumChannels;
+	int SampleRate;
+	int ByteRate;
+	short BlockAlign;
+	short BitsPerSample;
+	char Subchunk2ID[4];
+	int Subchunk2Size;
+	short *Data;
+}tipoWav;
+
+tipoWav readWAV(FILE *entradaWav, tipoWav wav){
+	/*Le o cabeçalho e informações sobreo formato do audio*/
+	fread(wav.ChunkID, 4, 1, entradaWav);
+	fread(&wav.ChunkSize, 4, 1, entradaWav);
+	fread(wav.Format, 4, 1, entradaWav);
+	fread(wav.Subchunk1ID, 4, 1, entradaWav);
+	fread(&wav.Subchunk1Size, 4, 1, entradaWav);
+	fread(&wav.AudioFormat, 2, 1, entradaWav);
+	fread(&wav.NumChannels, 2, 1, entradaWav);
+	fread(&wav.SampleRate, 4, 1, entradaWav);
+	fread(&wav.ByteRate, 4, 1, entradaWav);
+	fread(&wav.BlockAlign, 2, 1, entradaWav);
+	fread(&wav.BitsPerSample, 2, 1, entradaWav);
+	fread(wav.Subchunk2ID, 4, 1, entradaWav);
+	fread(&wav.Subchunk2Size, 4, 1, entradaWav);
+
+  	/*Aloca espaço em wav.Data do tamanho do espaço ocupado pelas amostras de audio (indicado em wav.Subchuk2Size)*/
+	wav.Data =  malloc(wav.Subchunk2Size);
+
+	/*Começa ler as amostras de audio*/
+	fread(wav.Data, 1, wav.Subchunk2Size, entradaWav);
+
+	fclose(entradaWav);
+	return wav;
 }
-headerWAV readWAV(FILE *input, headerWAV header){
-//Leitura do arquivo WAV
-fread(header.ChunkID, 1, 4, input);
-fread(&header.ChunkSize, 4, 1, input);
-fread(header.Format, 1, 4, input);
-fread(header.Subchunk1ID, 1, 4, input);
-fread(&header.Subchunk1Size, 4, 1, input);
-fread(&header.AudioFormat, 2, 1, input);
-fread(&header.NumChannels, 2, 1, input);
-fread(&header.SampleRate, 4, 1, input);
-fread(&header.ByteRate, 4, 1, input);
-fread(&header.BlockAlign, 2, 1, input);
-fread(&header.BitsPerSample, 2, 1, input);
-fread(header.Subchunk2ID, 1, 4, input);
-fread(&header.Subchunk2Size, 4, 1, input);
 
-short *Buffer = createData(input, header);
-header.Data = Buffer;
-fread(header.Data, header.BitsPerSample/8, header.Subchunk2Size/(header.BitsPerSample/8), input);
-fclose(input);
+tipoWav writeWAV(FILE *saidaWav, tipoWav wav){
+	/*Escreve o cabeçalho e informações sobreo formato do audio*/
+	fwrite(wav.ChunkID, 4, 1, saidaWav);
+	fwrite(&wav.ChunkSize, 4, 1, saidaWav);
+	fwrite(wav.Format, 4, 1, saidaWav);
+	fwrite(wav.Subchunk1ID, 4, 1, saidaWav);
+	fwrite(&wav.Subchunk1Size, 4, 1, saidaWav);
+	fwrite(&wav.AudioFormat, 2, 1, saidaWav);
+	fwrite(&wav.NumChannels, 2, 1, saidaWav);
+	fwrite(&wav.SampleRate, 4, 1, saidaWav);
+	fwrite(&wav.ByteRate, 4, 1, saidaWav);
+	fwrite(&wav.BlockAlign, 2, 1, saidaWav);
+	fwrite(&wav.BitsPerSample, 2, 1, saidaWav);
+	fwrite(wav.Subchunk2ID, 4, 1, saidaWav);
+	fwrite(&wav.Subchunk2Size, 4, 1, saidaWav);
 
-return header;
+	/*Começa escrever as amostras de audio*/
+	fwrite(wav.Data,  wav.BitsPerSample, wav.Subchunk2Size/wav.BitsPerSample, saidaWav);
+	// fclose(saidaWav);
+	return wav;
 }
-headerWAV writeWAV(FILE *output, headerWAV header){
-//Escrita do arquivo WAV
-fwrite(header.ChunkID, 1, 4, output);
-fwrite(&header.ChunkSize, 4, 1, output);
-fwrite(header.Format, 1, 4, output);
-fwrite(header.Subchunk1ID, 1, 4, output);
-fwrite(&header.Subchunk1Size, 4, 1, output);
-fwrite(&header.AudioFormat, 2, 1, output);
-fwrite(&header.NumChannels, 2, 1, output);
-fwrite(&header.SampleRate, 4, 1, output);
-fwrite(&header.ByteRate, 4, 1, output);
-fwrite(&header.BlockAlign, 2, 1, output);
-fwrite(&header.BitsPerSample, 2, 1, output);
-fwrite(header.Subchunk2ID, 1, 4, output);
-fwrite(&header.Subchunk2Size, 4, 1, output);
-
-short *Buffer = createData(output, header);
-header.Data = Buffer;
-fwrite(header.Data, header.BitsPerSample/8, header.Subchunk2Size/(header.BitsPerSample/8), output);
-fclose(output);
-
-return header;
-}		
 
 int main(){
-int i;
-headerWAV header, headerOut;
 
-FILE *input = fopen("adele.wav", "r"), *output = fopen("myFile.wav", "w");
+	FILE *entradaWav = fopen("adele.wav", "r");
+	if(!entradaWav) {
+		perror("Erro ao abrir arquivo de entrada");
+		exit(1);
+	}
 
-if(input == NULL)
- printf("Arquivo nao pode ser aberto!!!\n");
+	FILE *saidaWav = fopen("saida.wav", "w");
+	if(!saidaWav){
+		perror("Erro ao criar arquivo de saída");
+		exit(1);
+	}
 
-header = readWAV(input, header);
+	tipoWav wav;
+	wav = readWAV(entradaWav, wav);
+	writeWAV(saidaWav, wav);
 
-if(output == NULL)
- printf("Arquivo nao pode ser aberto!!!\n");
-
-headerOut = writeWAV(output, header);
-
-for(i = 0; i < 100; i++)
- printf("%d ", header.Data[i]);
-printf("\n");
-for(i = 0; i < 100; i++)
- printf("%d ", headerOut.Data[i]);
-printf("\n");
-for(i = 0; i < 4; i++)
- printf("%c ", headerOut.ChunkID[i]);
-printf("\n");
-system("PAUSE");
+	printf("\n");
 }
